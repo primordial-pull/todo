b47fd4fb0e8ad7ec1b999bfdeff2d5b75a977f11
primordial-pull <primordial.pull@gmail.com>
2026-03-25 15:09:29 +0900

    feat: 로고 클릭 시 홈으로 이동 기능 추가

--- src/components/GlobalNavBar.tsx
+++ src/components/GlobalNavBar.tsx
@@ -2,18 +2,26 @@
 
 import { useDeviceType } from '@/hooks/useDeviceType';
 import { Logo, LogoDoit } from './icons';
+import { useRouter } from 'next/navigation';
 
 export const GlobalNavBar = () => {
   const deviceType = useDeviceType();
+  const router = useRouter();
 
   const getLogo = () => {
     if (deviceType === null) return null;
     return deviceType === 'mobile' ? <Logo /> : <LogoDoit />;
   };
 
+  const handleLogoClick = () => {
+    router.push('/');
+  };
+
   return (
     <div className="w-full h-15 border-slate-200 border-b">
-      <div className="mt-[10px] ml-6 min-[1920px]:ml-[360px]"> {getLogo()}</div>
+      <div className="mt-[10px] ml-6 min-[1920px]:ml-[360px]" onClick={handleLogoClick}>
+        {getLogo()}
+      </div>
     </div>
   );
 };
