762646de58be090992279a1060e1ba236e3acaae
primordial-pull <primordial.pull@gmail.com>
2026-03-28 22:13:32 +0900

    feat: 페이지 별 메타데이터 추가

--- src/app/items/[itemId]/page.tsx
+++ src/app/items/[itemId]/page.tsx
@@ -1,5 +1,21 @@
 import { GlobalNavBar } from '@/components/GlobalNavBar';
 import { TodoDetail } from './_components/TodoDetail';
+import { Metadata } from 'next';
+import { fetchTodoItem } from '@/queries/TodoItem';
+
+type MetaDataProps = {
+  params: Promise<{ itemId: number }>;
+};
+
+export async function generateMetadata({ params }: MetaDataProps): Promise<Metadata> {
+  const { itemId } = await params;
+  const todo = await fetchTodoItem({ itemId });
+
+  return {
+    title: `${todo.name}`,
+    description: `${todo.name}를 확인하고 상태를 변경하세요.`,
+  };
+}
 
 export default async function Page({ params }: { params: Promise<{ itemId: string }> }) {
   const { itemId } = await params;
